f8e7f9fe00cf5348d85429a22937cf0f06602023
Christoph Li <christoph.li@nih.gov>
2026-05-04 17:51:20 -0400

    docs: version published documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 """Sphinx configuration for the LabArchives API documentation."""
 
+import re
 import sys
 from datetime import datetime
 from email.utils import getaddresses
@@ -7,7 +8,23 @@ from importlib.metadata import metadata as _metadata
 from importlib.metadata import version as _version
 from pathlib import Path
 
+_VERSION_TAG = r"^v(\d+\.\d+\.\d+)$"
+
 sys.path.insert(0, str((Path(__file__).resolve().parents[2] / "src").resolve()))
+
+
+def _set_release(_app, config) -> None:
+    match = re.fullmatch(_VERSION_TAG, getattr(config, "smv_current_version", ""))
+    if match:
+        release_name = match.group(1)
+        config.release = release_name
+        config.version = ".".join(release_name.split(".")[:2])
+
+
+def setup(app) -> None:
+    """Add small deployment helpers for versioned documentation builds."""
+    app.connect("config-inited", _set_release)
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -19,13 +36,14 @@ author_names = [name for name, _ in getaddresses([author])]
 
 copyright = f"{datetime.now().year}, {' and '.join(author_names)}"  # noqa: A001
 
-release = _version("labapi")
+release = _version(project)
 version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "sphinx_multiversion",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.extlinks",
@@ -49,6 +67,10 @@ language = "en"
 html_theme = "breeze"
 html_baseurl = "https://nimh-dsst.github.io/labapi/"
 html_static_path = ["_static"]
+
+# Versioned docs deployment
+smv_branch_whitelist = r"^$"
+smv_tag_whitelist = _VERSION_TAG
 
 # Intersphinx mapping
 intersphinx_mapping = {
